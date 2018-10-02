9fbb9bdc3df28f299ac134a13d4c535e496e399c
Javi Carrion <suavejavi@google.com>
2018-10-02 18:09:27 -0400

    Add BookView with properties to connect HTML

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 ($ => {
-
+// Model
     let Book = Backbone.Model.extend({
         defaults: {
             coverImage: "img/placeholder.png",
@@ -7,6 +7,21 @@
             author: "Tony Bicicleta",
             releaseDate: "2017",
             keywords: "JS Programing"
+        }
+    });
+
+    // View
+    let BookView = Backbone.View.extend({
+        tagName: 'div',
+        className: 'book-container',
+        template: $('#book-template').html(),
+
+        render: function () {
+            // template is a function that takes a JSON object and returns HTML
+            let template = _.template(this.template);
+            // this.el is what we defined in tagName. use $el to get access to JQuery html() function
+            this.$el.html(template(this.model.toJSON()));
+            return this;
         }
     });
 })(jQuery);
